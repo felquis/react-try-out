e53114ded34a4430795500e1fee180ca28d7d3a4
= <=>
2016-10-26 12:58:21 -0200

    Playing

---
+++ src/screens/login/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'react-toolbox/lib/button';
+
+export default function Login () {
+  return (
+    <div>
+      Ola <Button label="click me well" />
+    </div>
+  )
+}
